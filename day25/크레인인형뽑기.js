1c37a608a82002d5416ba181abcffbe1eb4e8ee3
SukbeomH <brent93.dev@gmail.com>
2022-04-15 10:09:02 +0900

    ~day26 algo

--- day25/크레인인형뽑기.js
+++ day25/크레인인형뽑기.js
@@ -25,20 +25,23 @@ moves = [1, 5, 3, 5, 1, 2, 1, 4];
 // 제거의 횟수를 센다
 width = board[0].length;
 depth = board.length;
+pick = moves.length;
 store = [];
 
-// 0이 아닌 값까지 내려간다
-for (let i = 0; i < depth; i++) {
-	// 무브의 길이만큼 반복한다
-	for (let j = 0; j < width; j++) {
-		// 무브값의 위치로 간다
-		if (board[i][moves[j] - 1] !== 0) {
-			// 새로운 배열에 값을 넣는다
-			store.push(board[i][moves[j] - 1] + 1);
-			// 원래 값 자리에 0을 넣는다
-			board[i].splice(moves[j] - 1, 1,"0") = 0;
-		}
-	}
+// 무브값의 위치로 간다, 무브의 길이만큼 반복
+for (let i = 0; i < moves.length; i++) {}
+// 0이 아닌 값까지 내려간다, 최대 board.length 까지 반복
+for (let j = 0; j < depth; j++) {}
+if (board[moves[0] - 1][1] !== 0) {
+	// 새로운 배열에 값을 넣는다
+	store.push(board[moves[j] - 1][i]);
+	// 원래 값 자리에 0을 넣는다
+	board[moves[j] - 1].map((e, idx) => {
+		if (idx === moves[j] - 1) e = 0;
+	});
 }
 
+aaa = board[4][0];
 store;
+aaa;
+board;
